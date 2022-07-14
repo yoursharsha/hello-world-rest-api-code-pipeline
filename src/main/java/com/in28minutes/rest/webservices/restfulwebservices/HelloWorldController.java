94f4ee9d3242cca45ca0607a28847aba5ab7853c
yoursharsha <41571745+yoursharsha@users.noreply.github.com>
2022-07-14 19:01:30 +0530

    Update HelloWorldController.java

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello World version 2";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
